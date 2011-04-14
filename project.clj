900006f60d655f8ae942b55987ed06a20d688601
Roman Scherer <roman.scherer@nugg.ad>
2011-04-14 14:11:21 +0200

    Release 0.0.1.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ring-cors "0.0.1-SNAPSHOT"
+(defproject ring-cors "0.0.1"
   :description "Ring middleware for Cross-Origin Resource Sharing."
   :url "http://github.com/r0man/ring-cors"
   :dependencies [[org.clojure/clojure "1.2.0"]
