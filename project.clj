ad9a004761c5a90726c60d60f72029480a74c315
Roman <roman@burningswell.com>
2012-04-16 20:12:48 +0200

    Switched to Clojure 1.4.0.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject ring-cors/ring-cors "0.0.4-SNAPSHOT"
   :description "Ring middleware for Cross-Origin Resource Sharing."
-  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]]
   :url "http://github.com/r0man/ring-cors"
   :min-lein-version "2.0.0")
