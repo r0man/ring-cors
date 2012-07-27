28317f7708be9f5dfd144812c7e7f47aea3b2ed0
roman <roman@bombaclaat.lan>
2012-07-27 19:47:00 +0200

    Started 0.0.6-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ring-cors/ring-cors "0.0.5"
+(defproject ring-cors/ring-cors "0.0.6-SNAPSHOT"
   :description "Ring middleware for Cross-Origin Resource Sharing."
   :url "http://github.com/r0man/ring-cors"
   :min-lein-version "2.0.0"
