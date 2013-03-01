9dd3cf7ec89fb624ec208749a7c8c23f5cc92982
Roman Scherer <roman.scherer@burningswell.com>
2013-03-01 20:31:56 +0100

    Updated dependencies.

--- project.clj
+++ project.clj
@@ -4,4 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.5.0"]])
