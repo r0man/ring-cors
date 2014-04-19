4794f59498bb32788fe3f76568751248fa109694
Roman Scherer <roman.scherer@burningswell.com>
2014-04-19 10:49:27 +0200

    Add lein-release option.

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
+  :lein-release {:deploy-via :clojars}
   :dependencies [[org.clojure/clojure "1.6.0"]])
