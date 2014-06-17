e8b007de1ff0a048837588fe78adfe686b5cc05c
Roman Scherer <roman.scherer@burningswell.com>
2014-06-17 17:28:41 +0200

    Remove :lein-release form project.clj

--- project.clj
+++ project.clj
@@ -4,7 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
-  :lein-release {:deploy-via :clojars}
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :deploy-repositories [["releases" :clojars]]
   :plugins [[jonase/eastwood "0.1.1"]
