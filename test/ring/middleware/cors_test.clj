r0man/ring-cors

commit bdb064ea88912fe792cd54f8f08b720b0a8e024a

Adding CORS header concerns

src/ring/middleware/cors.clj
@@ -1,10 +1,46 @@
 (ns ring.middleware.cors
   "Ring middleware for Cross-Origin Resource Sharing."
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.set :as set]))
 
 (defn origin
   "Returns the Origin request header."
   [request] (get (:headers request) "origin"))
+
+(defn preflight?
+  "Returns true if the request is a preflight request"
+  [request]
+  (= (request :request-method) :options))
+
+(defn string-to-set
+  "Transforms a comma-separated string to a set"
+  [the-string]
+  (set (map str/trim (str/split the-string #","))))
+
+(defn allow-preflight-headers?
+  "Returns true if the request is a preflight request and all the headers that
+  it's going to use are allowed. Returns false otherwise."
+  [request allowed-headers]
+  (if (= allowed-headers :any)
+    true
+    (let [headers (get-in request [:headers
+                                   "access-control-request-headers"] "")
+          headers-set (string-to-set headers)
+          allowed-headers-set (set (map name allowed-headers))]
+      (= (count allowed-headers)
+         (count (clojure.set/intersection allowed-headers-set headers-set))))))
+
+(defn allow-method?
+  "In the case of regular requests it checks if the request-method is allowed.
+  In the case of preflight requests it checks if the
+  access-control-request-method is allowed."
+  [request allowed-methods]
+  (let [preflight-name [:headers "access-control-request-method"]
+        request-method (if (preflight? request)
+                         (keyword (str/lower-case
+                                    (get-in request preflight-name "")))
+                         (:request-method request))]
+        (contains? allowed-methods request-method)))
 
 (defn allow-request?
   "Returns true if the request's origin matches the access control
@@ -12,12 +48,16 @@
   [request access-control]
   (let [origin (origin request)
         allowed-origins (:access-control-allow-origin access-control)
+        allowed-headers (:access-control-allow-headers access-control)
         allowed-methods (:access-control-allow-methods access-control)]
     (if (and origin
              (seq allowed-origins)
              (seq allowed-methods)
              (some #(re-matches % origin) allowed-origins)
-             (contains? allowed-methods (:request-method request)))
+             (if (preflight? request)
+               (allow-preflight-headers? request allowed-headers)
+               true)
+             (allow-method? request allowed-methods))
       true false)))
 
 (defn header-name
@@ -31,23 +71,59 @@
 (defn normalize-headers
   "Normalize the headers by converting them to capitalized strings."
   [headers]
+  (let [upcase #(str/join ", " (sort (map (comp str/upper-case name) %)))
+        to-header-names #(str/join ", " (sort (map (comp header-name name) %)))]
   (reduce
-   (fn [acc [k v]]
-     (assoc acc (header-name k)
-            (if (set? v)
-              (str/join ", " (sort (map (comp str/upper-case name) v)))
-              v)))
-   {} headers))
+    (fn [acc [k v]]
+      (assoc acc (header-name k)
+             (case k
+               :access-control-allow-methods (upcase v)
+               :access-control-allow-headers (to-header-names v)
+               v)))
+    {} headers)))
+
+(defn add-origin
+  "Add the access control headers using the request's origin to the response."
+  [request access-control response]
+  (let [allowed-methods (access-control :access-control-allow-methods)]
+    (if-let [origin (origin request)]
+      (update-in response [:headers] merge
+                 {:access-control-allow-origin origin
+                  :access-control-allow-methods allowed-methods})
+      response)))
+
+(defn add-allowed-headers
+  "Adds the allowed headers to the request"
+  [request access-control response]
+  (if (preflight? request)
+    (let [request-headers (get-in request
+                                  [:headers "access-control-request-headers"])
+          allowed-headers (access-control :access-control-allow-headers)
+          allowed-headers (if (= allowed-headers :any)
+                            (string-to-set request-headers)
+                            allowed-headers)]
+      (if allowed-headers
+        (update-in response [:headers] merge
+                   {:access-control-allow-headers allowed-headers})
+        response))
+    response))
+
 
 (defn add-access-control
-  "Add the access control headers using the request's origin to the response."
-  [request response access-control]
-  (if-let [origin (origin request)]
-    (update-in response [:headers] merge
-               (->> origin
-                    (assoc access-control :access-control-allow-origin)
-                    (normalize-headers)))
-    response))
+  "Add the access-control headers to the response based on the rules
+  and what came on the header."
+  [request access-control response]
+  (let [unnormalized-resp (->> response
+                               (add-origin request access-control)
+                               (add-allowed-headers request access-control))]
+    (update-in unnormalized-resp [:headers] normalize-headers)))
+
+(defn normalize-config
+  [access-control]
+  (-> (apply hash-map access-control)
+      (update-in [:access-control-allow-methods] set)
+      (update-in [:access-control-allow-headers] #(if (coll? %) (set %) %))
+      (update-in [:access-control-allow-origin] #(if (sequential? %) % [%]))))
 
 (defn wrap-cors
   "Middleware that adds Cross-Origin Resource Sharing headers.
@@ -59,25 +135,15 @@
          :access-control-allow-methods [:get :put :post :delete])))
 "
   [handler & access-control]
-  (let [access-control
-        (-> (apply hash-map access-control)
-            (update-in [:access-control-allow-methods] set)
-            (update-in [:access-control-allow-origin]
-                       #(if (sequential? %) % [%])))]
+  (let [access-control (normalize-config access-control)]
     (fn [request]
-      (if (and (= :options (:request-method request))
-               (allow-request?
-                (let [header (get (:headers request)
-                                  "access-control-request-method")]
-                  (assoc request :request-method
-                         (if header (keyword (str/lower-case header)))))
-                access-control))
-        {:status 200
-         :headers (->> (get (:headers request) "origin")
-                       (assoc access-control :access-control-allow-origin)
-                       (normalize-headers))
-         :body "preflight complete"}
+      (if (and (preflight? request) (allow-request? request access-control))
+        (let [blank-response {:status 200
+                              :headers {}
+                              :body "preflight complete"}]
+          (add-access-control request access-control blank-response))
         (if (origin request)
-          (when (allow-request? request access-control)
-            (add-access-control request (handler request) access-control))
+          (if (allow-request? request access-control)
+            (let [response (handler request)]
+              (add-access-control request access-control response)))
           (handler request))))))

test/ring/middleware/cors_test.clj
@@ -38,25 +38,55 @@
 (defn handler [request]
   ((wrap-cors (fn [_] {})
               :access-control-allow-origin #"http://example.com"
-              :access-control-allow-methods [:get :put :post])
+              :access-control-allow-headers #{:x-custom-header}
+              :access-control-allow-methods #{:get :put :post})
    request))
 
 (deftest test-preflight
-  (testing "success"
+  (testing "whitelist concrete headers"
+    (let [headers {"origin" "http://example.com"
+                   "access-control-request-method" "POST"
+                   "access-control-request-headers" "x-custom-header"}]
     (is (= {:status 200,
             :headers {"Access-Control-Allow-Origin" "http://example.com"
+                      "Access-Control-Allow-Headers" "X-Custom-Header"
                       "Access-Control-Allow-Methods" "GET, POST, PUT"}
             :body "preflight complete"}
            (handler {:request-method :options
                      :uri "/"
-                     :headers {"origin" "http://example.com"
-                               "access-control-request-method" "POST"}}))))
-  (testing "failure"
+                     :headers headers})))))
+
+  (testing "whitelist any headers"
+    (is (= {:status 200,
+            :headers {"Access-Control-Allow-Origin" "http://example.com"
+                      "Access-Control-Allow-Headers" "X-Bar, X-Foo"
+                      "Access-Control-Allow-Methods" "GET, POST, PUT"}
+            :body "preflight complete"}
+           ((wrap-cors (fn [_] {})
+                       :access-control-allow-origin #"http://example.com"
+                       :access-control-allow-headers :any
+                       :access-control-allow-methods #{:get :put :post})
+            {:request-method :options
+             :uri "/"
+             :headers {"origin" "http://example.com"
+                       "access-control-request-method" "POST"
+                       "access-control-request-headers" "x-foo, x-bar"}}))))
+
+  (testing "method not allowed"
     (is (nil? (handler
                {:request-method :options
                 :uri "/"
                 :headers {"origin" "http://example.com"
-                          "access-control-request-method" "DELETE"}})))))
+                          "access-control-request-method" "DELETE"}}))))
+
+  (testing "header not allowed"
+    (let [headers {"origin" "http://example.com"
+                   "access-control-request-method" "GET"
+                   "access-control-request-headers" "x-another-custom-header"}]
+    (is (nil? (handler
+               {:request-method :options
+                :uri "/"
+                :headers headers}))))))
 
 (deftest test-cors
   (testing "success"

commit 110f54637e2a81b076d6764467a6e5bf7556b1a7

Fix regression introduced in bdb064e
Also add failing test for it

src/ring/middleware/cors.clj
@@ -82,23 +82,20 @@
                v)))
     {} headers)))
 
-(defn add-origin
+(defn add-headers
   "Add the access control headers using the request's origin to the response."
   [request access-control response]
-  (let [allowed-methods (access-control :access-control-allow-methods)]
-    (if-let [origin (origin request)]
-      (update-in response [:headers] merge
-                 {:access-control-allow-origin origin
-                  :access-control-allow-methods allowed-methods})
-      response)))
+  (if-let [origin (origin request)]
+    (update-in response [:headers] merge
+               (assoc access-control :access-control-allow-origin origin))
+    response))
 
 (defn add-allowed-headers
   "Adds the allowed headers to the request"
-  [request access-control response]
+  [request allowed-headers response]
   (if (preflight? request)
     (let [request-headers (get-in request
                                   [:headers "access-control-request-headers"])
-          allowed-headers (access-control :access-control-allow-headers)
           allowed-headers (if (= allowed-headers :any)
                             (string-to-set request-headers)
                             allowed-headers)]
@@ -113,9 +110,12 @@
   "Add the access-control headers to the response based on the rules
   and what came on the header."
   [request access-control response]
-  (let [unnormalized-resp (->> response
-                               (add-origin request access-control)
-                               (add-allowed-headers request access-control))]
+  (let [allowed-headers (access-control :access-control-allow-headers)
+        rest-of-headers (dissoc access-control
+                                :access-control-allow-headers)
+        unnormalized-resp (->> response
+                               (add-headers request rest-of-headers)
+                               (add-allowed-headers request allowed-headers))]
     (update-in unnormalized-resp [:headers] normalize-headers)))
 
 (defn normalize-config

test/ring/middleware/cors_test.clj
@@ -121,3 +121,16 @@
              {:request-method :options
               :headers {"origin" "http://foo.com"}
               :uri "/"}))))
+
+(deftest additional-headers
+  (let [response ((wrap-cors (fn [_] nil)
+                             :access-control-allow-origin #".*"
+                             :access-control-allow-methods [:get]
+                             :access-control-expose-headers "Etag")
+                  {:request-method :get
+                   :uri "/"
+                   :headers {"origin" "http://example.com"}})]
+    (is (= {:headers {"Access-Control-Allow-Origin" "http://example.com"
+                      "Access-Control-Allow-Methods" "GET"
+                      "Access-Control-Expose-Headers" "Etag"}}
+           response))))