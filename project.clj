79a71a34e823abf3064def2ca8257b79a6a7c729
Roman Scherer <roman@burningswell.com>
2016-03-29 14:26:28 +0200

    Update dependencies

--- project.clj
+++ project.clj
@@ -4,15 +4,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [ring/ring-core "1.3.2"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [ring/ring-core "1.4.0"]]
   :deploy-repositories [["releases" :clojars]]
-  :plugins [[jonase/eastwood "0.2.1"]
-            [lein-bikeshed "0.1.6"]
+  :plugins [[jonase/eastwood "0.2.3"]
+            [lein-bikeshed "0.3.0"]
             [lein-difftest "2.0.0"]]
   :eastwood {:exclude-linters [:bad-arglists]}
-  :aliases {"ci" ["do" ["with-profile" "dev:dev,1.4:dev,1.5:dev" "difftest"] ["lint"]]
-            "lint" ["do" ["bikeshed"] ["eastwood"]]}
-  :profiles
-  {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-   :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}})
+  :aliases {"ci" ["do" ["difftest"] ["lint"]]
+            "lint" ["do" ["bikeshed"] ["eastwood"]]})
