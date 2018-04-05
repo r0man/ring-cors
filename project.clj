6c2bd59c714031eeff50bf8dd6c9c223345e6d97
r0man <roman@burningswell.com>
2018-04-05 17:39:01 +0200

    Update dependencies

--- project.clj
+++ project.clj
@@ -4,10 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]]
   :deploy-repositories [["releases" :clojars]]
-  :plugins [[jonase/eastwood "0.2.4"]
-            [lein-bikeshed "0.4.1"]
+  :plugins [[jonase/eastwood "0.2.5"]
+            [lein-bikeshed "0.5.1"]
             [lein-difftest "2.0.0"]]
   :eastwood {:exclude-linters [:bad-arglists]}
   :aliases {"ci" ["do" ["difftest"] ["lint"]]
